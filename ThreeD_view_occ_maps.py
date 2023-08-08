8236a80495e4ecc580270e61f3a01934f91f5d44
Cezary Wawrzyniak <cezary.waw99@gmail.com>
2023-08-08 21:24:33 +0200

    new variations of showing occupancy maps

--- ThreeD_view_occ_maps.py
+++ ThreeD_view_occ_maps.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-if len(sys.argv) != 2:
-    print("Usage: python script.py folder_directory")
-    sys.exit(1)
+# if len(sys.argv) != 2:
+#     print("Usage: python script.py folder_directory")
+#     sys.exit(1)
 
 # Get the folder directory from command-line argument
-folder_path = sys.argv[1]
+# folder_path = sys.argv[1]
+folder_path = '/home/czarek/mgr/3D_data/train/images'
 
 # Check if the folder exists
 if not os.path.isdir(folder_path):
@@ -31,14 +32,16 @@ file_path = os.path.join(folder_path, random_file)
 occ_map = np.load(file_path)
 
 # Downsample the occupancy map
-downsample_factor = 4  # Adjust the factor as desired
-downsampled_occ_map = occ_map[::downsample_factor, ::downsample_factor, ::downsample_factor]
+# downsample_factor = 4  # Adjust the factor as desired
+# downsampled_occ_map = occ_map[::downsample_factor, ::downsample_factor, ::downsample_factor]
 
 # Create a mask for the occupied places
-occupied_mask = downsampled_occ_map.astype(bool)
+# occupied_mask = downsampled_occ_map.astype(bool)
+occupied_mask = occ_map.astype(bool)
 
 # Create a color array for the occupied places (set to black)
-color = np.zeros(downsampled_occ_map.shape + (4,))
+# color = np.zeros(downsampled_occ_map.shape + (4,))
+color = np.zeros(occ_map.shape + (4,))
 color[occupied_mask] = (0, 0, 0, 0.2)  # Set RGB values to (0, 0, 0) and alpha (transparency) to 0.5
 
 # Visualize the 3D occupancy map with semi-transparent objects (without wireframe edges)
@@ -49,9 +52,12 @@ indices = np.nonzero(occ_map)
 ax.voxels(occupied_mask, facecolors=color, edgecolors=color)
 
 # ax.scatter(indices[0], indices[1], indices[2], c='k', marker='o')
-ax.set_xlim(0, occ_map.shape[0]/4)
-ax.set_ylim(0, occ_map.shape[1]/4)
-ax.set_zlim(0, occ_map.shape[2]/4)
+# ax.set_xlim(0, occ_map.shape[0]/4)
+# ax.set_ylim(0, occ_map.shape[1]/4)
+# ax.set_zlim(0, occ_map.shape[2]/4)
+ax.set_xlim(0, occ_map.shape[0])
+ax.set_ylim(0, occ_map.shape[1])
+ax.set_zlim(0, occ_map.shape[2])
 
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
