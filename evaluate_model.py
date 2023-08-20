e52f223690e7d083d9cdadf09ac8cf24eaf49767
Cezary Wawrzyniak <cezary.waw99@gmail.com>
2023-08-20 18:54:21 +0200

    better visualisation of evaluation

--- evaluate_model.py
+++ evaluate_model.py
@@ -3,13 +3,9 @@ import torch
 from pathlib import Path
 from train import UNet_cooler, MapsDataModule
 import matplotlib.pyplot as plt
-import numpy as np
-from torch import nn
-from torchviz import make_dot
-from torchview import draw_graph
 
 MODEL_PATH = "sampling_cnn_vol3_32.pth"
-BASE_PATH = Path('/home/czarek/mgr/eval_data/test')
+BASE_PATH = Path('/home/czarek/mgr/eval_data')
 
 model = UNet_cooler()
 model.load_state_dict(torch.load(MODEL_PATH))
@@ -42,24 +38,55 @@ image_show = image_show.transpose((0, 2, 3, 1))
 
 with torch.no_grad():
     output = model(image, coords)
-    clipped = torch.clamp(output, min=-5, max=1)
+    clipped = torch.clamp(output, min=-3, max=1)
     # print(model)
 # make_dot(output, params=dict(list(model.named_parameters()))).render("torchviz", format="png")
 
 # model_graph = draw_graph(model, input_data=(image, coords), expand_nested=True, save_graph=True, filename='torchview')
 
-f, axarr = plt.subplots(1, 4)
+x_start = coords.data.tolist()[0][0][0][0]
+y_start = coords.data.tolist()[0][0][0][1]
+x_finish = coords.data.tolist()[0][0][1][0]
+y_finish = coords.data.tolist()[0][0][1][1]
+start = (y_start, x_start)
+finish = (y_finish, x_finish)
+
+
+# f, axarr = plt.subplots(1, 4)
+# x_np = image.detach().cpu().numpy()
+# x_np = x_np.transpose((0, 2, 3, 1))
+#
+# y_np = mask.detach().cpu().numpy()
+# y_np = y_np.transpose((0, 2, 3, 1))
+#
+# y_hat_np = output.detach().cpu().numpy()
+# y_hat_np = y_hat_np.transpose((0, 2, 3, 1))
+#
+# clipped = clipped.detach().cpu().numpy()
+# clipped = clipped.transpose((0, 2, 3, 1))
+#
+# axarr[0].imshow(x_np[0])
+# axarr[0].scatter(x_start, y_start, c='g', marker='o', s=30, label='Start')
+# axarr[0].scatter(x_finish, y_finish, c='r', marker='o', s=30, label='Finish')
+# axarr[1].imshow(y_np[0])
+# axarr[2].imshow(y_hat_np[0])
+# axarr[3].imshow(clipped[0])
+# plt.show()
+
+
+f, axarr = plt.subplots(1, 3)
 x_np = image.detach().cpu().numpy()
 x_np = x_np.transpose((0, 2, 3, 1))
+
 y_np = mask.detach().cpu().numpy()
 y_np = y_np.transpose((0, 2, 3, 1))
-y_hat_np = output.detach().cpu().numpy()
-y_hat_np = y_hat_np.transpose((0, 2, 3, 1))
+
 clipped = clipped.detach().cpu().numpy()
 clipped = clipped.transpose((0, 2, 3, 1))
-axarr[0].imshow(x_np[0])
-axarr[1].imshow(y_np[0])
-axarr[2].imshow(y_hat_np[0])
-axarr[3].imshow(clipped[0])
-plt.show()
 
+axarr[0].imshow(x_np[0])
+axarr[0].scatter(x_start, y_start, c='g', marker='o', s=300, label='Start')
+axarr[0].scatter(x_finish, y_finish, c='r', marker='o', s=300, label='Finish')
+axarr[1].imshow(y_np[0])
+axarr[2].imshow(clipped[0])
+plt.show()
