4ca11a7bb64de9e1366b27095070ac7a04269b19
Cezary Wawrzyniak <cezary.waw99@gmail.com>
2023-08-26 01:01:28 +0200

    update to statistical 3D testing

--- ThreeD_test_network_and_algorithms.py
+++ ThreeD_test_network_and_algorithms.py
@@ -12,7 +12,7 @@ from ThreeD_generate_paths import astar
 from ThreeD_rrt_star import RRTStar
 from ThreeD_neural_rrt import RRTStar as NeuralRRTStar
 
-BASE_PATH = Path('/home/czarek/mgr/3D_eval_data/test/oneshot')
+BASE_PATH = Path('/home/czarek/mgr/3D_eval_data')
 MODEL_PATH = "/home/czarek/mgr/models/3D_sampling_cnn_vol2_47.pth"
 MAX_ITERATIONS = 5000
 GOAL_THRESHOLD = 3.0
